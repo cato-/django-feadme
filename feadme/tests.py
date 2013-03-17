908af714d0019340d3ac417be73b627922ef771c
allo <allo@laxu.de>
2013-03-17 17:31:11 +0100

    added registration tests.

--- feadme/tests.py
+++ feadme/tests.py
@@ -1,16 +1,53 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.core import mail
+from registration.models import RegistrationProfile
+from django_hosts.reverse import reverse_full
+from feadme_project import settings
 
-Replace this with more appropriate tests for your application.
-"""
+def assertRedirect(testcase, response, target_url):
+    testcase.assertEquals(response.status_code, 302)
+    testcase.assertIn(target_url, response['Location'], 
+        "the redirect target should be {expected}, but is {actual}".format(
+            expected=target_url,
+            actual=response['Location']
+        )   
+    )
 
-from django.test import TestCase
+class RegistrationTest(TestCase):
 
+    def setUp(self):
+        self.form_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'testpass',
+            'password2': 'testpass',
+        }
+        self.c = Client()
+        self.register_url = reverse('registration.views.register')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_registration_should_redirect_to_registration_complete(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test that the registration redirects to the registration complete view.
         """
-        self.assertEqual(1 + 1, 2)
+        correct_redirect_url = reverse("registration_complete")
+
+        response = self.c.post(self.register_url, self.form_data)
+        assertRedirect(self, response, correct_redirect_url)
+
+    def test_registration_should_create_registrationprofile(self):
+        response = self.c.post(self.register_url, self.form_data)
+        self.assertEqual(RegistrationProfile.objects.all().count(), 1)
+
+    def test_mail_contains_activation_url(self):
+        response = self.c.post(self.register_url, self.form_data)
+        activation_url = reverse_full("www", "registration.views.activate", 
+            view_kwargs={
+            'activation_key': RegistrationProfile.objects.all()[0].activation_key
+            }
+        )
+        print activation_url
+
+        response = self.c.post(self.register_url, self.form_data)
+        self.assertEqual(len(mail.outbox), 1)
+        assert activation_url in mail.outbox[0].body
